5abd483590a1b0bf35c777729384343ce6b2c92c
Rakhmanov-03 <omadjonrahmonov588@gmail.com>
2023-11-11 21:47:03 +0500

    Ifga oid masalalar

---
+++ main.js
@@ -0,0 +1,148 @@
+// if1
+// let a=+prompt("sonni kiriting")
+// if (a>0){
+//     console.log(a+1);
+// }else{
+//     console.log(a);
+// }
+// if2
+// let a=+prompt("sonni kiriting")
+// if (a>0){
+//     console.log(a+1);
+// }else{
+//     console.log(a-2);
+// }
+// if3
+// let a=+prompt("sonni kiriting")
+// if (a>0){
+//     console.log(a+1);
+// }
+// else if (a==0){
+//     console.log(10);
+// }
+// else{
+//     console.log(a+2);
+// }
+// if4
+// let a=+prompt("a ni kiriting")
+// let b=+prompt("b ni kiriting")
+// let c=+prompt("c ni kiriting")
+// if (a>0){
+//     console.log(a=1);
+// }else{
+//     console.log(a=0);
+// }
+// if(b>0){
+//     console.log(b=1);
+// }else{
+//     console.log(b=0);
+// }
+// if(c>0){
+//     console.log(c=1);
+// }else{
+//     console.log(c=0);
+// }
+// let jami=a+b+c
+// console.log(jami+" ta musbat");
+// if5
+// let a=+prompt("a ni kiriting")
+// let b=+prompt("b ni kiriting")
+// let c=+prompt("c ni kiriting")
+// if (a>0){
+//     console.log(a=1);
+// }else{
+//     console.log(a=0);
+// }
+// if(b>0){
+//     console.log(b=1);
+// }else{
+//     console.log(b=0);
+// }
+// if(c>0){
+//     console.log(c=1);
+// }else{
+//     console.log(c=0);
+// }
+// let musbat=a+b+c
+// let manfiy=3-musbat
+// console.log(musbat+" ta musbat",manfiy+" ta manfiy");
+// if6
+// let a=+prompt("a ni kiriting")
+// let b=+prompt("b ni kiriting")
+// if (a>b){
+//     console.log(a);
+// }else{console.log(b);}
+// if8
+// let a=+prompt("a ni kiriting")
+// let b=+prompt("b ni kiriting")
+// if (a>b){
+//     console.log(a,b);
+// }else{
+//     console.log(b,a);
+// }
+// if10
+// let a=+prompt("a ni kiriting")
+// let b=+prompt("b ni kiriting")
+// if(a==b){
+//     console.log(0);
+// }else{
+//     console.log(a+b);
+// }
+// if11
+// let a=+prompt("a ni kiriting")
+// let b=+prompt("b ni kiriting")
+// if(a==b){
+//     console.log(0);
+// }
+// else if(a>b){
+//     console.log(a);
+// }else{
+//     console.log(b);
+// }
+// if12
+// let a=+prompt("a ni kiriting")
+// let b=+prompt("b ni kiriting")
+// let c=+prompt("c ni kiriting")
+// if(a>b){
+//     console.log(b);
+// }
+// else if(a>c){
+//     console.log(c);
+// }
+// else{
+//     console.log(a);
+// }
+// if13
+// let a=+prompt("a ni kiriting")
+//  let b=+prompt("b ni kiriting")
+//  let c=+prompt("c ni kiriting")
+//  if(a>b==b>c){
+//     console.log(b);
+//  }
+//  else if(b>a==a>c){
+// console.log(a);
+//  }
+//  else{console.log(c);}
+// if14
+// let a=+prompt("a ni kiriting")
+// let b=+prompt("b ni kiriting")
+// if(a>b){
+//     console.log(b,a);
+// }else{
+//     console.log(a,b);
+// }
+// if15
+// let a=+prompt("a ni kiriting")
+// let b=+prompt("b ni kiriting")
+// let c=+prompt("c ni kiriting")
+// if((a+b)>(b+c)){
+//     console.log(a,b);
+// }
+// else if((b+c)>(a+c)){
+//     console.log(b,c);
+// }
+// else if((a+c)>(a+b)){
+//     console.log(a,c);
+// }
+
+
